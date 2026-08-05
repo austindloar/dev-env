34fc71aeefb88ac5d08c348bd6ae04d34b75efdf
Austin Loar <austind5717@gmail.com>
2026-08-05 15:25:58 -0400

    Change indentation based on file type

--- .config/nvim/lua/austin/init.lua
+++ .config/nvim/lua/austin/init.lua
@@ -1,2 +1,25 @@
 require("austin.set")
 require("austin.lsp")
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "astro",
+        "css",
+        "eruby",
+        "html",
+        "htmlangular",
+        "htmldjango",
+        "javascriptreact",
+        "less",
+        "sass",
+        "scss",
+        "svelte",
+        "typescriptreact",
+        "vue"    
+    },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end
+})
